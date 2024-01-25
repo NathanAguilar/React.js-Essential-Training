ea058bc0bd5806dacbb1653358d60b7e522602d1
NathanAguilar <127912919+NathanAguilar@users.noreply.github.com>
2024-01-25 11:36:35 -0800

    Handling Forms in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useState } from "react";
+
+function useInput(initialValue) {
+  const [value, setValue] =
+    useState(initialValue);
+  return [
+    {
+      value,
+      onChange: (e) => setValue(e.target.value)
+    },
+    () => setValue(initialValue)
+  ];
+}
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    checked => !checked, 
-    false
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] =
+    useState("#000000");
+  const submit = (e) => {
+    e.preventDefault();
+    alert(
+      `${titleProps.value}, ${colorProps.value}`
     );
+    resetTitle();
+    resetColor();
+  };
   return (
-    <div className="App">
-     <input 
-       type="checkbox"
-       value={checked}
-       onChange={ setChecked}
+    <form onSubmit={submit}>
+      <input
+        {...titleProps}
+        type="text"
+        placeholder="color title..."
       />
-     <label>
-      {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+      <input {...colorProps} type="color" />
+      <button>ADD</button>
+    </form>
   );
 }
 
 export default App;
+
